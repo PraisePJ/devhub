9eb447a2e1f243208e502336fa6977a0d8eff51b
PraisePJ <93436815+PraisePJ@users.noreply.github.com>
2023-04-07 14:09:14 +0100

    Update settings.py

--- SchoolPortal/settings.py
+++ SchoolPortal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5=)))vw-eycurlc3+u(xiv3eilnqx2^@nbw$5j_fi9e-918o)s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['devhub-1zsz.onrender.com']
 
