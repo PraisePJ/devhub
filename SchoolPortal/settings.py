11c81abbb750e00d549339b2dc3363c3d0772bbb
PraisePJ <93436815+PraisePJ@users.noreply.github.com>
2023-04-07 13:49:23 +0100

    Update settings.py

--- SchoolPortal/settings.py
+++ SchoolPortal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5=)))vw-eycurlc3+u(xiv3eilnqx2^@nbw$5j_fi9e-918o)s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['devhub-1zsz.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['devhub-1zsz.onrender.com']
 
 
 # Application definition
